04248ccf0c177ec34ec6ee7874bee440641e6c1f
MarteBiesmans <n-abbe@hotmail.com>
2017-04-20 13:24:26 +0200

    opgave 10

--- matlab/voorbeeld_oef10.m
+++ matlab/voorbeeld_oef10.m
@@ -1,8 +1,8 @@
 import bisection.*;
 clear;
-n=100;
-range1 = [1 50]; %range of integers
-range2 = [-25, 25];
-A = full(gallery('tridiag', n, randi(range1, 1, n-1), randi(range2, 1, n), randi(range1, 1, n-1)));
-E_echt = eig(A);
-E_bis = bisection(A, 0, 5, 10^(-1))
+n=10;
+vector = rand(1, n-1)
+A = full(gallery('tridiag', vector, rand(1, n), vector));
+kappa = cond(A);
+E_echt = sort(eig(A));
+E_bis = bisection(A, 0, 1, 10^(-10));
