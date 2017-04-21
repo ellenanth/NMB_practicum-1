7ed5437971b0d200ff86ac798318bcf74b0fa2af
ellenanth <anthonissen.ellen@gmail.com>
2017-04-21 14:30:21 +0200

    oef5 :(

--- matlab/oef5.m
+++ matlab/oef5.m
@@ -1,5 +1,13 @@
+clear;
+maxit = 100;
 load('mat1.txt');
-disp(mat1);
+[m,n] = size(mat1);
+
 [e,res] = qr_shiftrayleigh(mat1);
-n = size(res,2);
-plot(1:n, res,'bo:');
+k = size(res,2);
+X_1 = 1:k;
+Y_1 = res;
+%plot(X_1,Y_1,'bo:');
+
+[V,D] = gelijktijdige_it(mat1,eye(n,m),maxit);
+disp(diag(D));
